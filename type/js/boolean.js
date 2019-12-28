59715292a7e639fdaadf6aeea1ffc31ba06d55cb
Tadashi Hashimoto <sakusennx@gmail.com>
2019-12-28 11:33:32 +0900

    falsyな値とtruthyな値についての説明を追加

--- type/js/boolean.js
+++ type/js/boolean.js
@@ -6,3 +6,18 @@ var bool2 = false;
 document.write('<h3>数値の比較は==</h3>');
 document.write('<p>' + (10 == 10) + '</p>');
 document.write('<p>' + (15 < 8) + '</p>');
+
+
+
+
+//falsyな値
+
+false;
+undefined;
+null;
+0;
+NaN;
+""; //（空文字列）
+
+//truthyな値。
+//falsyで無い値はすべてtruthy
